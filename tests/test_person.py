916af706b97804eef85ba8c2750d160cdb44a89e
Julie <jawarren89@gmail.com>
2022-04-01 13:30:32 -0700

    Added Person class and Movie class. Created tests for for Person.

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,23 @@
+from logging.handlers import WatchedFileHandler
 import pytest
+from viewing_party.person import Person
 
-def test_1():
+def test_person():
     # Arrange
+    name = "Ada"
+    watched = ["Euphoria"]
+    friends = []
+    subscriptions = ["hulu", "HBO max"]
 
     # Act
+    ada = Person(name, watched, friends, subscriptions)
 
     # Assert
-    pass
+    assert ada.name == name
+    assert len(ada.watched) == 1
+    assert ada.watched == ["Euphoria"]
+    assert "Euphoria" in ada.watched
+    assert len(ada.friends) == 0
+    assert len(ada.subscriptions) == 2
+    assert ada.subscriptions[0] == "hulu"
+    assert ada.subscriptions[1] == "HBO max"
--- viewing_party/movie.py
+++ viewing_party/movie.py
@@ -1,2 +1,10 @@
 class Movie:
-    pass
+    
+    def __init__(self, title, genre, rating, hosts):
+        self.title = title
+        self.genre = genre
+        self.rating = rating
+        self.hosts = hosts
+
+    def update_rating(self, new_rating):
+        self.rating = new_rating
